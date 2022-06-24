4967e4a1e56c8425cb5a547e9e746b3c881623b5
Yu Jong Cheul <51194504+whdcjf96@users.noreply.github.com>
2022-06-24 09:56:06 +0900

    다시

---
+++ 01_개발/bodycare/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="content-wrapper" class="d-flex flex-column">
+    <HeaderCom></HeaderCom>
+    <FooterCom></FooterCom>
+  </div>
+</template>
+
+<script>
+
+import HeaderCom from '@/components/HeaderCom.vue'
+import FooterCom from '@/components/FooterCom.vue'
+export default {
+  name: "HomeView",
+  components: {
+    HeaderCom,
+    FooterCom
+  }
+
+}
+</script>
+
+<style>
+
+</style>
