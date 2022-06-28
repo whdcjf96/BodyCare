4c129b9088c18904d0bc75699fe1b00686c7622b
wlqm00 <qkrwlals007@naver.com>
2022-06-28 14:02:33 +0900

    박지민
    User 수정

--- 01_개발/BodyCare_BackEnd/src/main/java/com/example/bodycare_backend/model/User.java
+++ 01_개발/BodyCare_BackEnd/src/main/java/com/example/bodycare_backend/model/User.java
@@ -18,17 +18,14 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class Register {
+public class User {
 //    Long(객체), long(일반자료형) => 8byte 정수형
     private Long id; // 회원아이디
-    private String firstName; // 회원 이름
-    private String lastName; // 회원 성
+    private char gender; // 성별
+    private float height; // 키
+    private String name; // 회원 이름
     private String email;  // email
-    private String phone;  // 핸드폰 번호
-    private String deleteYn; // 삭제 여부
-    private String insertTime; // 생성날짜
-    private String updateTime; // 수정날짜
-    private String deleteTime; // 삭제날짜
+    private String password; // 비빌번호
 }
 
 
