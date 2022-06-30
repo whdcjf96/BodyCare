11c223bd3822d54f3812d59282765cb0ca4f76d1
oggn <99378862+oggn@users.noreply.github.com>
2022-06-30 12:38:47 +0900

    김창권

---
+++ 01_개발/BodyCare_BackEnd/src/main/java/com/example/bodycare_backend/model/dkdk.java
@@ -0,0 +1,15 @@
+package com.example.bodycare_backend.model;
+
+/**
+ * packageName : com.example.bodycare_backend.model
+ * fileName : dkdk
+ * author : 4571c
+ * date : 2022-06-30
+ * description :
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-06-30         4571c          최초 생성
+ */
+public class dkdk {
+}
