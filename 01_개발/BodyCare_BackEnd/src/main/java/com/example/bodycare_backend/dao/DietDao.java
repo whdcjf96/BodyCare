dfe8b40f0d086fe672471c7c9660a8d65deffd24
oggn <99378862+oggn@users.noreply.github.com>
2022-07-01 16:42:34 +0900

    김창권

--- 01_개발/BodyCare_BackEnd/src/main/java/com/example/bodycare_backend/dao/DietDao.java
+++ 01_개발/BodyCare_BackEnd/src/main/java/com/example/bodycare_backend/dao/DietDao.java
@@ -2,8 +2,10 @@ package com.example.bodycare_backend.dao;
 
 import com.example.bodycare_backend.model.Diet;
 import com.example.bodycare_backend.paging.Criteria;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * packageName : com.example.bodycare_backend.dao
@@ -16,15 +18,19 @@ import java.util.List;
  * -----------------------------------------------------------
  * 2022-06-30         jc          최초 생성
  */
+@Mapper
 public interface DietDao {
 
-//    인풋창에서 조회
+    //    인풋창에서 조회
     List<Diet> selectAll(Criteria criteria);
 
-//    인풋창에서 타이틀별로 검색
+    //    인풋창에서 타이틀별로 검색
     List<Diet> selectByTitle(Criteria criteria);
 
-//    음식 추가
+    // id 로 회원 조회 메소드
+    Optional<Diet> selectById(Long id);
+
+    //    음식 추가
     Long insertDiet(Diet diet);
 
 }
