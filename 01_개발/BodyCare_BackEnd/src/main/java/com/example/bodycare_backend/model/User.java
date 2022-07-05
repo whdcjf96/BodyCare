bb65421eeebc517b220cbf6cab773c4679376916
Yu Jong Cheul <51194504+whdcjf96@users.noreply.github.com>
2022-07-05 15:22:35 +0900

    유종철
    로그인

--- 01_개발/BodyCare_BackEnd/src/main/java/com/example/bodycare_backend/model/User.java
+++ 01_개발/BodyCare_BackEnd/src/main/java/com/example/bodycare_backend/model/User.java
@@ -3,6 +3,12 @@ package com.example.bodycare_backend.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * packageName : com.example.bodycare_backend.model
@@ -18,7 +24,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class User {
+public class User implements UserDetails {
     private Long id;
     private String email;
     private String password;
@@ -28,5 +34,43 @@ public class User {
     private String createdTime;
     private String updateTime;
     private int age;
+    private String roles;
+    // Serialize 네트워크 전송 자동 생성 번호 => 수동으로 이름을 명시함
+    // 보안 관련해서 자동 번호 생성
+    private static final long serialVersionUID = 1L;
+    // 권한 관련된 정보를 얻는 메소드
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // 권한 정보를 담을 배열 정의
+        ArrayList<GrantedAuthority> auth = new ArrayList<>();
+        // SimpleGrantedAuthority 권한 객체를 넣기 : auth 배열
+        auth.add(new SimpleGrantedAuthority(roles));
 
+        return auth;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
